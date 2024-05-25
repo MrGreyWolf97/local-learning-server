18905e50f2abb908f5bea270390cbf07139bbd2c
mr_grey_wolf <enrico.cestino@live.it>
2024-05-25 11:34:57 +0200

    added card-component (experimenting with component structure)

--- local/src/main/java/com/learning/ManageBeanTest.java
+++ local/src/main/java/com/learning/ManageBeanTest.java
@@ -3,9 +3,9 @@ package com.learning;
 import javax.faces.bean.*;
 
 @ManagedBean(name = "manageBeanTest", eager = true)
+@SessionScoped
 public class ManageBeanTest {
 
-      
    public ManageBeanTest() {
       System.out.println("ManageBeanTest started!");
    }
